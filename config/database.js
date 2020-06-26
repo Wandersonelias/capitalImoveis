f40b1077a4ce354995aa1e0d3170bfda0db7fe0c
Wanderson Elias Coelho dos Santos <wandersonelias55@gmail.com>
2020-06-25 23:16:09 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -13,21 +13,7 @@ const Sequelize = require('sequelize');
         }).catch((e)=>{
             console.log("Erro: " + e);
         });
-        /*
-        const db = new Sequelize(
-            'capital', //capital
-            'wanderson',//wandersonelias
-            'regina',{ //regina10
-            host: 'localhost', // 'mysql669.umbler.com', //'localhost',
-            dialect: 'mysql'
-        });
-        db.authenticate().then(()=>{
-            console.log("Conectado com sucesso");
-            
-        }).catch((e)=>{
-            console.log("Erro: " + e);
-        });
-        */
+       
 
 
 
